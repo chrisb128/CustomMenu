112ae953c2228e7a53f82a39c0ad1d226100bc45
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 09:16:45 -0600

    Initial Commit

---
+++ src/main/java/info/chrisb/custommenu/CustomMenu.java
@@ -0,0 +1,17 @@
+package info.chrisb.custommenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomMenu extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
